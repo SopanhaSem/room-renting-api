1896c923dc624d96483592ad6557ca9cd62a3b50
Sem Sopanha <semsopanha072@gmail.com>
2024-11-18 20:03:21 +0700

    fix postgres

---
+++ src/main/java/co/istad/roomrenting/RoomRentingApplication.java
@@ -0,0 +1,13 @@
+package co.istad.roomrenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRentingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/istad/roomrenting/RoomRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.roomrenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
